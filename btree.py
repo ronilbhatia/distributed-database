7281ef3069f4b9c976595e6c4953322350681a9f
ronilbhatia <ronilbhatia@gmail.com>
2018-11-18 12:59:14 -0800

    Add comments to btree insertion method

--- btree.py
+++ btree.py
@@ -28,7 +28,9 @@ class Node:
         return len(self.children) == 0
 
     def add_key(self, key):
+        # Check if node is leaf - if so attempt to add
         if self.is_leaf():
+            # Check if node is full - if not add key directly on
             if not self.is_full():
                 is_inserted = False
                 for i in range(self.count()):
@@ -38,9 +40,12 @@ class Node:
                         break
                 if not is_inserted:
                     self.keys.append(key)
+            # if the node is full we need to split it
             else:
                 return self.split(key)
         else:
+            # if the node is not a leaf we must find the appropriate
+            # child to attempt to add the key to
             for i in range(self.count()):
                 found_child = False
                 if key < self.keys[i]:
@@ -48,13 +53,18 @@ class Node:
                     res = self.children[i].add_key(key)
                     child = i
                     break
+            # if we didn't find a child it must be the last child
             if not found_child:
                 res = self.children[-1].add_key(key)
                 child = len(self.children) - 1
+            # if we have a res that implies the child had to be split
             if res:
                 median = res['median']
                 left = res['left']
                 right = res['right']
+                # If the node has space we can add the median value on
+                # We will add the left and right nodes as children of
+                # the node on either side of the median value
                 if not self.is_full():
                     is_inserted = False
                     for i in range(self.count()):
@@ -65,15 +75,20 @@ class Node:
                             self.children.insert(i+1, right)
                             is_inserted = True
                             break
+                    # if we didn't insert, assume the median value will
+                    # fill the last available spot in the node, and same
+                    # for the children (left and right nodes)
                     if not is_inserted:
                         self.keys.append(median)
                         del self.children[child]
                         self.children.append(left)
                         self.children.append(right)
+                # If the node does not have space we must split it. We
+                # must not lose track of the left and right nodes though
+                # so we can remove the original child node (since it has
+                # been split) and insert the left & right nodes as
+                # children, passing the median value to split the node
                 else:
-                    # child = self.children[child]
-                    # child.keys = [median]
-                    # child.children = [left, right]
                     del self.children[child]
                     self.children.insert(child, left)
                     self.children.insert(child+1, right)
@@ -96,20 +111,35 @@ class Node:
         right.children = self.children[mid_idx+1:]
         return {'median': median, 'left': left, 'right': right}
 
+    def remove_key(self, key):
+
+
 node = BTree()
-node.add_key(7)
-node.add_key(6)
-node.add_key(5)
-node.add_key(4)
-node.add_key(3)
-node.add_key(2)
-node.add_key(1)
-# node.add_key(2)
-# node.add_key(3)
-# node.add_key(4)
-# node.add_key(5)
-# node.add_key(6)
 # node.add_key(7)
+# node.add_key(6)
+# node.add_key(5)
+# node.add_key(4)
+# node.add_key(3)
+# node.add_key(2)
+# node.add_key(4)
+# node.add_key(1)
+# node.add_key(5)
+# node.add_key(2)
+# node.add_key(10)
+# node.add_key(11)
+# # node.add_key(11)
+# node.add_key(3)
+# node.add_key(6)
+# node.add_key(9)
+# node.add_key(7)
+# node.add_key(8)
+node.add_key(1)
+node.add_key(2)
+node.add_key(3)
+node.add_key(4)
+node.add_key(5)
+node.add_key(6)
+node.add_key(7)
 
 pdb.set_trace()
 print(node.count())
